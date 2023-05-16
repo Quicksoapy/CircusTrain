6bb9a904705e4171fe0509e1e0ddf131b6ed879c
maya <73587977+Quicksoapy@users.noreply.github.com>
2023-05-16 11:48:50 +0200

    unittest 2

--- CircusTrein.UnitTests/Tests.cs
+++ CircusTrein.UnitTests/Tests.cs
@@ -5,7 +5,7 @@ public class Tests
     [SetUp]
     public void Setup()
     {
-        
+
     }
 
     [Test]
@@ -20,4 +20,27 @@ public class Tests
 
         Assert.AreNotEqual(expectedAnimals[0], animals[0]);
     }
+
+    [Test]
+    public void AnimalCollection_Correctly_Sorts_All_Animals_In_Lists()
+    {
+        var expectedAnimals = new List<Animal>
+        {
+            new Animal(AnimalType.Herbivore, AnimalSize.Large),
+            new Animal(AnimalType.Herbivore, AnimalSize.Medium),
+            new Animal(AnimalType.Herbivore, AnimalSize.Small),
+            new Animal(AnimalType.Carnivore, AnimalSize.Large),
+            new Animal(AnimalType.Carnivore, AnimalSize.Medium),
+            new Animal(AnimalType.Carnivore, AnimalSize.Small)
+        };
+        var animalCollection = new AnimalCollection(expectedAnimals);
+        
+        Assert.AreNotEqual(animalCollection.GetLargeHerbivores()[0], new Animal(AnimalType.Herbivore, AnimalSize.Large));
+        Assert.AreNotEqual(animalCollection.GetMediumHerbivores()[0], new Animal(AnimalType.Herbivore, AnimalSize.Medium));
+        Assert.AreNotEqual(animalCollection.GetSmallHerbivores()[0], new Animal(AnimalType.Herbivore, AnimalSize.Small));
+        Assert.AreNotEqual(animalCollection.GetLargeCarnivores()[0], new Animal(AnimalType.Carnivore, AnimalSize.Large));
+        Assert.AreNotEqual(animalCollection.GetMediumCarnivores()[0], new Animal(AnimalType.Carnivore, AnimalSize.Medium));
+        Assert.AreNotEqual(animalCollection.GetSmallCarnivores()[0], new Animal(AnimalType.Carnivore, AnimalSize.Small));
+    }
+
 }
